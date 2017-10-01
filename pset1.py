dd89c785f10b39e4bf50a509411cad5264104f01
Joseph Day <joeday@cs.uchicago.edu>
2017-10-01 13:40:58 -0500

    pset1

--- pset1.py
+++ pset1.py
@@ -60,46 +60,77 @@ works[:,T-1] = 0
 con[:,T-1] = float(R)*agrid + w[T-1]*works[:,T-1] + b*(1-works[:,T-1]) - sav[:,T-1]
 V[:,T-1] = u(con[:,T-1])
 
+
+
 ## Solve for Value Fn Backwards
 
-for it in range(T-2,-1,-1):
-  print('Solving value function at age {}'.format(it))
-  for ia in range(0, na):
-    cash = float(R) * agrid[ia] + w[it]*works[ia,it] + b*(1-works[ia,it])
-    c = np.maximum((cash-agrid), 1*10**-10)
-    Vchoice = u(c) + beta * V[:,it+1] - psi * works[:,it+1]
-    V[ia,it] = np.max(Vchoice)
-    savind[ia,it]  = np.argmax(Vchoice)
-    sav[ia,it] = agrid[np.int_(savind[ia,it])]
-    con[ia,it] = cash - sav[ia,it]
-    if w[it] > b:
-      works[ia,it-1] = 1
-    else:
-      works[ia,it-1] = 0
 
+solve = True
+simulate = True
+plot = True
+
+if solve:
+  for it in range(T-2,-1,-1):
+    print('Solving value function at age {}'.format(it))
+    for ia in range(0, na):
+      cash_working = float(R) * agrid[ia] + w[it]
+      c_working = np.maximum((cash_working-agrid), 1*10**-10)
+
+      cash_not_working = float(R) * agrid[ia] + b
+      c_not_working = np.maximum((cash_not_working-agrid), 1*10**-10)
+      
+      Vchoice_working = u(c_working) + beta * V[:,it+1] - psi 
+      Vchoice_not_working = u(c_not_working) + beta * V[:,it+1] 
+
+      working_max = np.max(Vchoice_working)
+      not_working_max = np.max(Vchoice_not_working)
+      if working_max > not_working_max:
+        V[ia,it] = working_max
+        savind[ia,it]  = np.argmax(Vchoice_working)
+        sav[ia,it] = agrid[np.int_(savind[ia,it])]
+        con[ia,it] = cash_working - sav[ia,it]
+        works[ia,it] = 1
+        print('ya better work bitch')
+      else:   
+        V[ia,it] = not_working_max
+        savind[ia,it]  = np.argmax(Vchoice_not_working)
+        sav[ia,it] = agrid[np.int_(savind[ia,it])]
+        con[ia,it] = cash_not_working - sav[ia,it]
+        works[ia,it] = 0
+        print('you dont gotta go to work work work work')
+
+print('sup')
 
 ## Simulate
-a_in_sim = np.zeros(T+1)
-a_initial = 0
-inter = interpolate.interp1d(agrid,range(0,na),'nearest')
-a_in_sim[0] = inter(a_initial)
-
-for it in range(0,T):
-  print(' Simulating time period {}'.format((it+1)))
-  a_in_sim[it+1] = savind[np.int_(a_in_sim[it]), it]
-  asim = agrid[np.int_(a_in_sim)]
-  csim = R*asim[0:T] + w - asim[1:(T+1)]
+if simulate:
+  a_in_sim = np.zeros(T+1)
+  a_initial = 0
+  inter = interpolate.interp1d(agrid,range(0,na),'nearest')
+  a_in_sim[0] = inter(a_initial)
 
 
-fig = plt.figure(1)
-plt.subplot(1,2,1)
-plt.plot(range(1,51), w, 'k-', lw=1)
-plt.plot(range(1,51), csim, 'r--', lw=1)
-plt.grid
-plt.title('Income and Consumption')
-plt.legend('Income', 'Consumption')
+  # create array of income after choice of labor
+  true_income = w
 
-plt.subplot(1,2,2)
-plt.plot(range(0,51), asim, 'b-', lw=1)
-plt.plot(agrid,np.zeros(na), 'k-', lw=0.5)
+  for it in range(0,T):
+    print(' Simulating time period {}'.format((it+1)))
+    a_in_sim[it+1] = savind[np.int_(a_in_sim[it]), it]
+    asim = agrid[np.int_(a_in_sim)] 
+    if it==25:
+      print(asim)
+      
+    csim = R*asim[0:T] + true_income - asim[1:(T+1)]
+
+if plot:
+  fig = plt.figure(1)
+  plt.subplot(1,2,1)
+  plt.plot(range(1,51), w, 'k-', lw=1)
+  plt.plot(range(1,51), csim, 'r--', lw=1)
+  plt.grid
+  plt.title('Income and Consumption')
+  plt.legend('Income', 'Consumption')
+
+  plt.subplot(1,2,2)
+  plt.plot(range(0,51), asim, 'b-', lw=1)
+  plt.plot(agrid,np.zeros(na), 'k-', lw=0.5)
 
